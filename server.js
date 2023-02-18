00017e13d6b688bc36a6bf9dbed8b0b521e33cf1
Maartje Oostvogels <moostvogels@hotmail.com>
2023-02-18 15:03:42 +0000

    Catch errors to prevent server from stopping

--- server.js
+++ server.js
@@ -16,52 +16,71 @@ app.get('/api/users', async (req, res) => {
 // Get a single user by id
 app.get('/api/users/:id', async (req, res) => {
   const id = req.params.id;
-  const user = await User.findById(id).exec();
-  if (!user) {
-    res.status(404).json('User not found');
+
+  try {
+    const user = await User.findById(id).exec();
+
+    if (user) {
+      return res.json(user);
+    } else {
+      res.status(404).json('User not found');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Something went wrong' });
   }
-  else {
-    res.json(user);
-  }
-})
+});
 
 // Create a user
 app.post('/api/users', async (req, res) => {
   const { username, email } = req.body;
 
-  if (username && email) {
+  try {
     const user = new User({ username, email });
     await user.save();
-
+    
     res.json(user);
-  } else {
-    res.json({
-      error: 'error, please provide a username and email for the user'
-    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Something went wrong' });
   }
 });
 
 // Update a user
 app.put('/api/users/:id', async (req, res) => {
   const id = req.params.id;
-  const user = await User.findById(id);
-  if (!user) {
-    res.status(404).json('User not found');
-  }
-  else {
-    user.username = req.body.username;
-    user.email = req.body.email;
-    await user.save();
-    res.json(user);
+
+  try {
+    const user = await User.findById(id);
+
+    if (user) {
+      const { username, email } = req.body;
+      user.username = username;
+      user.email = email;
+      await user.save();
+
+      res.json(user);
+    } else {
+      res.status(404).json('User not found');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Something went wrong' });
   }
 });
 
 // Delete a user
 app.delete('/api/users/:id', async (req, res) => {
   const id = req.params.id;
-  await User.findOneAndDelete(id).exec();
-  res.status(204).send();
-})
+
+  try {
+    await User.findByIdAndDelete(id).exec();
+    res.status(204).send();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
